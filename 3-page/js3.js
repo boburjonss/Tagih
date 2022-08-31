a06b469d28b9e13fda95362003af34619dc10cc6
Hojiakbar2009 <108812258+Hojiakbar2009@users.noreply.github.com>
2022-08-31 11:56:57 +0500

    0.85 full bitdi

---
+++ 3-page/js3.js
@@ -0,0 +1,12 @@
+// let sl = document.querySelectorAll(".sl")
+// slider = document.querySelectorAll(".slider")
+
+
+// slider.forEach((item)=>{
+//     if(sl == true){
+//         slider.style.display = 'block'
+//     }
+// })
+
+
+// console.log(sl);
